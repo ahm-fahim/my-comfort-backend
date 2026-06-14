954048225431703810af07203f0329d5abc1dba1
ahm-fahim <contactahmfahim@gmail.com>
2026-06-14 15:18:43 +0600

    feat: added user registration and login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,37 @@
 import userModel from "../models/userModel.js";
 import validator from "validator";
 import bycrpt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+//  Create Token 
+const createToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+}
+
 // Route for user login
 const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await userModel.findOne({ email });
+        
+        if (!user) {
+            return res.json({success: false, message: "User not found"});
+        }
+
+        const isMatch = await bycrpt.compare(password, user.password);
+
+        if (isMatch) {
+            const token = createToken(user._id);
+            res.json({success: true, message:"Login successful", token})
+        } else {
+            res.json({success: false, message:"Invalid password"});
+        }
+
+    } catch (error) { 
+        console.log(error);
+        res.json({ success: false, message: "Error occurred while logging in user" });
+    }
 
 }
 
@@ -30,11 +59,19 @@ const registerUser = async (req, res) => {
         // hashing user password
         const salt = await bycrpt.genSalt(10);
         const hashedPassword = await bycrpt.hash(password, salt);
-        
 
+        const newUser = new userModel({
+            name, email, password: hashedPassword
+        });
+        
+        const  user  = await newUser.save();
+        const token = createToken(user._id);
+        
+        res.json({ success: true, message: "User registered successfully", token });
 
     } catch (error) {
-
+        console.log(error);
+        res.json({ success: false, message: "Error occurred while registering user" });
     }
 }
 
