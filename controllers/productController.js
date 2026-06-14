720bdd9656fbe292e60cb3b7aa631961ea520f7f
ahm-fahim <contactahmfahim@gmail.com>
2026-06-14 16:33:59 +0600

    feat: added multer middleware for image uploads

---
+++ controllers/productController.js
@@ -0,0 +1,42 @@
+
+
+// add product
+const addProduct = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+// list product
+const listProduct = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+// remove product
+const removeProduct = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+// single product details
+const singleProduct = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+
+export { addProduct, listProduct, removeProduct, singleProduct };
+
+
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv/config"
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import userRouter from './routes/userRoute.js';
+import productRouter from './routes/productRoute.js';
 
 // App Config 
 const app = express();
@@ -18,6 +19,8 @@ app.use(cors());
 
 // API endpoints
 app.use('/api/user', userRouter)
+app.use('/api/product',productRouter)
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 })
