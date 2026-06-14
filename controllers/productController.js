3e39a1f124650c2e5a8c7870451ba0d385135ae6
ahm-fahim <contactahmfahim@gmail.com>
2026-06-14 21:53:21 +0600

    feat: added cloudinary image uploader

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,38 +1,65 @@
+import { v2 as cloudinary } from 'cloudinary';
 
+// Configure Cloudinary using your .env variables
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET_KEY 
+});
 
 // add product
 const addProduct = async (req, res) => {
     try {
-        
+        const { name, description, price, category, subCategory, sizes, bestseller } = req.body;
+
+        const image1 = req.files.image1 && req.files.image1[0];
+        const image2 = req.files.image2 && req.files.image2[0];
+        const image3 = req.files.image3 && req.files.image3[0];
+        const image4 = req.files.image4 && req.files.image4[0];
+
+        const images = [image1, image2, image3, image4].filter((item) => item !== undefined);
+
+        let imagesUrl = await Promise.all(
+            images.map(async (item) => {
+                let result = await cloudinary.uploader.upload(item.path, { resource_type: "image" });
+                return result.secure_url;
+            }))
+
+        console.log(name, description, price, category, subCategory, sizes, bestseller);
+        console.log(imagesUrl);
+
+        res.json({});
+
     } catch (error) {
-        
+        console.log(error);
+        res.json({ success: false, message: error.message });
     }
 }
 
 // list product
 const listProduct = async (req, res) => {
     try {
-        
+
     } catch (error) {
-        
+
     }
 }
 
 // remove product
 const removeProduct = async (req, res) => {
     try {
-        
+
     } catch (error) {
-        
+
     }
 }
 
 // single product details
 const singleProduct = async (req, res) => {
     try {
-        
+
     } catch (error) {
-        
+
     }
 }
 
