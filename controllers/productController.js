73bc44336620539f6af65f8cc9e8694309f39ec3
ahm-fahim <contactahmfahim@gmail.com>
2026-06-15 12:48:55 +0600

    feat: added api/product/remove api

--- controllers/productController.js
+++ controllers/productController.js
@@ -60,8 +60,11 @@ const listProduct = async (req, res) => {
 const removeProduct = async (req, res) => {
     try {
 
+        await productModel.findByIdAndDelete(req.body.id);
+        res.json({ success: true, message: "Product removed successfully" })
     } catch (error) {
-
+        console.log(error);
+        res.json({ success: false, message: error.message });
     }
 }
 
